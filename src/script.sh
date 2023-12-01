c16a01fcc478d6b1bf37597a1bbee171fe4a29ee
Werner van der Merwe <wvdm1217@gmail.com>
2023-12-01 15:51:30 +0200

    feat: reduce unnecessary headers in curl

--- src/script.sh
+++ src/script.sh
@@ -13,14 +13,7 @@ while true; do
 
         curl --silent \
         --location 'https://www.property24.com/search/counter' \
-        -H 'authority: www.property24.com' \
-        -H 'accept: */*' \
-        -H 'accept-language: en-US,en;q=0.9' \
-        -H 'content-type: application/json; charset=UTF-8' \
-        -H 'cookie: P24UUEYED=Id%3Dzbkye4zp0lsh0xecm4uxszgm%26Date%3D638349764208899461;' \
-        -H 'dnt: 1' \
-        -H 'origin: https://www.property24.com' \
-        -H 'request-id: |4dG+t.QipiL' \
+        -H 'content-type: application/json;' \
         --data-raw '{"bedrooms":0,"bathrooms":0,"availability":0,"rentalRates":[],"sizeFrom":{"isCustom":false,"value":null},"sizeTo":{"isCustom":false,"value":null},"erfSizeFrom":{"isCustom":false,"value":null},"erfSizeTo":{"isCustom":false,"value":null},"floorSizeFrom":{"isCustom":false,"value":null},"floorSizeTo":{"isCustom":false,"value":null},"parkingType":1,"parkingSpaces":0,"hasFlatlet":null,"hasGarden":null,"hasPool":null,"furnishedStatus":2,"isPetFriendly":null,"isRepossessed":null,"isRetirement":null,"isInSecurityEstate":null,"onAuction":null,"onShow":null,"propertyTypes":[4,5,6],"autoCompleteItems":[{"id":459,"name":"Stellenbosch","parentId":null,"parentName":"Western Cape","type":2,"source":0,"normalizedName":"stellenbosch"}],"searchContextType":1,"priceFrom":{"isCustom":false,"value":null},"priceTo":{"isCustom":false,"value":null},"searchType":1,"sortOrder":0,"developmentSubType":0}' \
         --compressed
 
