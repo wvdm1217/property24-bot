dee136c1b985851aefa5be2489da4a966b97f1db
Werner van der Merwe <wvdm1217@gmail.com>
2023-12-02 19:22:57 +0000

    fix: change script back to 1 minute intervals

--- src/script.sh
+++ src/script.sh
@@ -8,43 +8,45 @@ else
     prev_count=0;
     echo "Container running for the first time. Creating count.txt file with value 0";
 fi
+while true; do
+    response=$( \
 
-response=$( \
+        curl --silent \
+        --location 'https://www.property24.com/search/counter' \
+        -H 'content-type: application/json;' \
+        --data-raw '{"bedrooms":0,"bathrooms":0,"availability":0,"rentalRates":[],"sizeFrom":{"isCustom":false,"value":null},"sizeTo":{"isCustom":false,"value":null},"erfSizeFrom":{"isCustom":false,"value":null},"erfSizeTo":{"isCustom":false,"value":null},"floorSizeFrom":{"isCustom":false,"value":null},"floorSizeTo":{"isCustom":false,"value":null},"parkingType":1,"parkingSpaces":0,"hasFlatlet":null,"hasGarden":null,"hasPool":null,"furnishedStatus":2,"isPetFriendly":null,"isRepossessed":null,"isRetirement":null,"isInSecurityEstate":null,"onAuction":null,"onShow":null,"propertyTypes":[4,5,6],"autoCompleteItems":[{"id":459,"name":"Stellenbosch","parentId":null,"parentName":"Western Cape","type":2,"source":0,"normalizedName":"stellenbosch"}],"searchContextType":1,"priceFrom":{"isCustom":false,"value":null},"priceTo":{"isCustom":false,"value":null},"searchType":1,"sortOrder":0,"developmentSubType":0}' \
+        --compressed
 
-    curl --silent \
-    --location 'https://www.property24.com/search/counter' \
-    -H 'content-type: application/json;' \
-    --data-raw '{"bedrooms":0,"bathrooms":0,"availability":0,"rentalRates":[],"sizeFrom":{"isCustom":false,"value":null},"sizeTo":{"isCustom":false,"value":null},"erfSizeFrom":{"isCustom":false,"value":null},"erfSizeTo":{"isCustom":false,"value":null},"floorSizeFrom":{"isCustom":false,"value":null},"floorSizeTo":{"isCustom":false,"value":null},"parkingType":1,"parkingSpaces":0,"hasFlatlet":null,"hasGarden":null,"hasPool":null,"furnishedStatus":2,"isPetFriendly":null,"isRepossessed":null,"isRetirement":null,"isInSecurityEstate":null,"onAuction":null,"onShow":null,"propertyTypes":[4,5,6],"autoCompleteItems":[{"id":459,"name":"Stellenbosch","parentId":null,"parentName":"Western Cape","type":2,"source":0,"normalizedName":"stellenbosch"}],"searchContextType":1,"priceFrom":{"isCustom":false,"value":null},"priceTo":{"isCustom":false,"value":null},"searchType":1,"sortOrder":0,"developmentSubType":0}' \
-    --compressed
+    );
 
-);
+    count=$(echo $response | jq '.count');
 
-count=$(echo $response | jq '.count');
-
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to parse response";
-    break;
-fi
-
-if [ $count -ne $prev_count ]; then
-    echo $count;
-    echo $count > count.txt;
-
-    if [ $count -gt $prev_count ]; then
-        message="New property added in Stellenbosch. Count: $count";
-        echo $message;
-
-        send_message \
-        "$TELEGRAM_TOKEN" \
-        "$TELEGRAM_CHAT_ID" \
-        "$message";
-
-    else
-        echo "Property removed in Stellenbosch. Count: $count";
+    if [ $? -ne 0 ]; then
+        echo "Error: Failed to parse response";
+        break;
     fi
 
-    prev_count=$count;
+    if [ $count -ne $prev_count ]; then
+        echo $count;
+        echo $count > count.txt;
 
-fi
+        if [ $count -gt $prev_count ]; then
+            message="New property added in Stellenbosch. Count: $count";
+            echo $message;
 
-echo -n ".";
+            send_message \
+            "$TELEGRAM_TOKEN" \
+            "$TELEGRAM_CHAT_ID" \
+            "$message";
+
+        else
+            echo "Property removed in Stellenbosch. Count: $count";
+        fi
+
+        prev_count=$count;
+
+    fi
+
+    echo -n ".";
+    sleep 60;
+done
