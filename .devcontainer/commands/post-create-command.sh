1ee3d0bd3f2ade95ae6b1d6331a1e5a2d907dcc7
Werner van der Merwe <werner@spatialedge.ai>
2025-09-28 13:09:05 +0000

    feat: add pre-commit configuration and install hooks in post-create command

--- .devcontainer/commands/post-create-command.sh
+++ .devcontainer/commands/post-create-command.sh
@@ -2,4 +2,11 @@
 
 echo "Starting: Post Create Command"
 
+# UV Sync
+uv sync --dev
+source .venv/bin/activate
+
+# Install pre-commit hooks
+pre-commit install
+
 echo "Finishing: Post Create Command"
